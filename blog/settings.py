1152f6787b66bb5d840ec4702bd0a5ad4f792201
Chinmaya Bindhani <153605374+Chinmaya-Bindhani@users.noreply.github.com>
2026-06-09 23:59:52 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER':os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
